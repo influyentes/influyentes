56251b3e1ba4600e42a850fae7ff9385101b1f3e
Mohamed Ziata <wakematta@gmail.com>
2016-10-23 16:09:32 +0200

    Adding content to the landing

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
-  def block_feature(title, text, link, icon)
-    content_tag :div, class: 'col-md-3 col-sm-6 col-xs-6 col-xxs-12' do
-      link_to link, class: 'landing-feature to-animate' do
+  def block_feature(title:, text:, icon:, block_class: nil)
+    content_tag :div, class: "col-md-5 col-sm-6 col-xs-6 col-xxs-12 #{block_class}" do
+      content_tag :div, class: 'landing-feature to-animate' do
         content_tag(:span, icon, class: 'landing-feature-icon') +
         content_tag(:h3, title, class: 'landing-feature-lead') +
         content_tag(:p, text, class: 'landing-feature-text')
@@ -9,8 +9,8 @@ module HomeHelper
     end
   end
 
-  def block_benefit(title, text)
-    content_tag :div, class: 'col-md-4 col-sm-6 landing-text-wrap text-center' do
+  def block_benefit(title:, text:, block_class: nil)
+    content_tag :div, class: "col-md-4 col-sm-6 landing-text-wrap text-center #{block_class}" do
       content_tag :div, class: 'landing-text to-animate' do
         content_tag(:h3, title, class: 'landing-uppercase-sm') +
         content_tag(:p, text)
